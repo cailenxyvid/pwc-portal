f89e44b129d0ba6125fbe4d00d40dd996fe28bd2
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-04-03 16:42:58 -0400

    .

--- src/routes/replay/+page.svelte
+++ src/routes/replay/+page.svelte
@@ -10,7 +10,7 @@
 </script>
     
   
-<div class="container h-full mx-auto flex justify-center pt-10 pl-10 pr-10 border border-secondary-500">
+<div class="container h-full mx-auto flex justify-center pt-10 pl-10 pr-10 border border-secondary-500 space-x-6">
 	{#each events as event}
 	<ReplayEvent {event} />
 	{/each}
