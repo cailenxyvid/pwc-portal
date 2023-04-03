c6bc83928b5e7f3166bf0ba98a0c4b19a0ab5db5
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-04-03 16:34:49 -0400

    display events in rows on small screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,7 +92,7 @@
 	<div class="w-full text-center mb-10">
 		<button on:click={registerEvents} class="{enableRegister ? 'bg-primary-500' : 'bg-primary-100'} text-white rounded-sm p-2 text-xl">Register for selected events</button>
 	</div>
-	<div class="w-full flex flex-row space-x-6">
+	<div class="w-full flex flex-col md:flex-row space-x-6">
 		{#each events as event}
 		<UpcomingEvent {event} {toggleEvent} />
 		{/each}
