f4a6c69ce77b5cbae4af633022e4142b204fc9ed
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-05-17 20:47:33 -0400

    force rerender if query string changes (if search results had overlap, previous results showed previous highlighting)

--- src/lib/components/SearchEventCard.svelte
+++ src/lib/components/SearchEventCard.svelte
@@ -4,7 +4,7 @@
     export let event:Event;
     
     let query: string | null;
-    
+
     const highlight = (text:string) => {                
         if (!query) return;
         let queryString = ' '+query;
@@ -15,9 +15,11 @@
     $: (query = $page.url.searchParams.get('queryString'))
 </script>
 
+{#key query}
 <div class="card p-2 overflow-scroll overflow-ellipsis text-xs">
     <a class="block" href="/event/{event.id}">{@html highlight(event.title)}</a>
     <div class="prose">
         {@html highlight(event.content)}
     </div>
 </div>
+{/key}
