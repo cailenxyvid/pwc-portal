b370923eed2cf460edbc429f254e3a82021605ea
JoshuaXyvid <jmccloskey@xyvid.com>
2023-05-18 13:22:22 -0500

    Update SearchBar Added Padding Changed transition to blur.
    Update SearchBar Added Padding Changed transition to blur.

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import { slide } from "svelte/transition";
+    import { blur } from "svelte/transition";
     let show = false;
     const toggle = () => { show = !show }
 </script>
 
-<button on:click={toggle}><i class="inline-block fa fa-search"></i></button>
+<button on:click={toggle}><i class="inline-block fa fa-search pt-5 pb-5"></i></button>
 {#if show}
-<form action="/search" class="inline-block ml-4" transition:slide>
+<form action="/search" class="inline-block ml-4" transition:blur>
     <input type="text" name="queryString" />
     <button class="variant-filled-primary p-2">Go</button>
 </form>
