fcdb8e62383daa394351f74807c7eb3a05f3288a
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-05-18 16:21:22 -0400

    style tweaks

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -4,11 +4,12 @@
 </script>
 
 <div class="card p-2">
-    <div class="card-header">
+    <div class="card-header2">
         <img src="{event.image_url}" alt="{event.title}" class="block w-full h-auto">
         <a href="/event/{event.id}">{event.title}</a>
     </div>    
-    <div class="event-speakers">
+    <div class="event-speakers italic my-4">
+        <div class="underline font-extrabold">Featured speakers:</div>
         {@html event.content_speakers}
     </div>
     <div class="event-replay-notice">
