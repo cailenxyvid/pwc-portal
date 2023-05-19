d5b3bf36b152920f88ff2ec81d2bf53711bf95d6
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-05-19 11:43:44 -0400

    faq footer

--- src/routes/(portal)/faq/+page.svelte
+++ src/routes/(portal)/faq/+page.svelte
@@ -16,3 +16,25 @@
     <div class="text-sm">{@html faq.content}</div>
 	{/each}
 </div>
+
+<div class="footer bg-gray-800 text-white w-full p-2">
+    <div class="flex flex-row gap-24">
+        <div>
+            Have additional questions?
+            <span class="text-xl block">
+                <a href="http://us_trust_leadership_institute_inbox@pwc.com">Contact us</a> to 
+                learn more.
+            </span>
+        </div>
+        <div>
+            <b class="text-xl block">Jonathan Block</b>
+            Managing Director, PwC
+        </div>
+        <div>
+            <b class="text-xl block">Megan Conway</b>
+            Managing Director, PwC
+        </div>
+    </div>
+    <div class="text-xs w-full mt-6">© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure for further details. This content is for general purposes only, and should not be used as a substitute for consultation with professional advisors.
+    </div>
+</div>
