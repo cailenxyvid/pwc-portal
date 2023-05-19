03fe4d4cc6387f15d638f3a9db1490a23058c99c
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-05-19 11:04:32 -0400

    html in faq content

--- src/routes/(portal)/faq/+page.svelte
+++ src/routes/(portal)/faq/+page.svelte
@@ -13,6 +13,6 @@
 <div class="container h-full mx-auto justify-center pt-10 pl-10 pr-10 space-y-12">
 	{#each faqs as faq}
 	<h3 id="faq-{faq.id}">{faq.title}</h3>
-    <div class="text-sm">{faq.content}</div>
+    <div class="text-sm">{@html faq.content}</div>
 	{/each}
 </div>
