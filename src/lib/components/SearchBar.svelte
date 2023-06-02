42e972b246836625021e0d9b3a24270377e5d0a6
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-02 13:57:38 -0400

    search text color

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -7,7 +7,7 @@
 <button on:click={toggle}><i class="inline-block fa fa-search pt-5 pb-5"></i></button>
 {#if show}
 <form action="/search" class="inline-block ml-1 sm:ml-4" transition:blur>
-    <input type="text" name="queryString" class="w-20 md:w-48" />
+    <input type="text" name="queryString" class="w-20 md:w-48 text-black" />
     <button class="variant-filled-primary p-2">Go</button>
 </form>
 {/if}
