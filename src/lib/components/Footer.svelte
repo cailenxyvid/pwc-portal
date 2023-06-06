231735e6ef59c50ffb037f91af998dac48a657d1
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-06 16:30:09 -0400

    footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<div class="mt-12 bg-[#2d2d2d] p-12">
+    <div class="flex flex-col md:flex-row justify-between">
+        <div class="text-white">
+            Have additional questions?
+            <span class="text-xl block">
+                <a href="mailto:us_trust_leadership_institute_inbox@pwc.com">Contact us</a> to 
+                learn more.
+            </span>
+        </div>
+        <div class="flex flex-row gap-4">
+            <img src="/jonathan-block.jpg" alt="" class="inline h-24">
+            <div class="t">
+                <h2 class="text-white">Jonathan Block</h2>
+                <div class="text-white">Communications and Change Leader, PwC US</div>
+            </div>			
+        </div>
+        <div class="flex flex-row gap-4">
+            <img src="/megan-conway.jpg" alt="" class="inline h-24">
+            <div class="t">
+                <h2 class="text-white">Megan Conway</h2>
+                <div class="text-white">Managing Director, Trust Leadership Institute</div>
+            </div>			
+        </div>
+    </div>
+    <div class="text-xs text-white w-full mt-6">
+        © 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure for further details. This content is for general purposes only, and should not be used as a substitute for consultation with professional advisors.
+    </div>
+</div>
--- src/routes/(portal)/faq/+page.svelte
+++ src/routes/(portal)/faq/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ReplayEvent from '$lib/components/ReplayEvent.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	import type { PageData } from './$types';
 
@@ -19,24 +19,4 @@
 	{/each}
 </div>
 
-<div class="footer bg-[#2d2d2d] text-white w-full p-2 mt-10">
-    <div class="flex flex-row gap-24">
-        <div>
-            Have additional questions?
-            <span class="text-xl block">
-                <a href="mailto:us_trust_leadership_institute_inbox@pwc.com">Contact us</a> to 
-                learn more.
-            </span>
-        </div>
-        <div>
-            <b class="text-xl block">Jonathan Block</b>
-            Managing Director, PwC
-        </div>
-        <div>
-            <b class="text-xl block">Megan Conway</b>
-            Managing Director, PwC
-        </div>
-    </div>
-    <div class="text-xs w-full mt-6">© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure for further details. This content is for general purposes only, and should not be used as a substitute for consultation with professional advisors.
-    </div>
-</div>
+<Footer />
