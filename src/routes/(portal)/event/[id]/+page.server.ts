91b87fefadf7c4db6037602b2e96c43e49b7007d
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-08 16:29:48 -0400

    quick fix for actions on single event view

--- src/routes/(portal)/event/[id]/+page.server.ts
+++ src/routes/(portal)/event/[id]/+page.server.ts
@@ -1,10 +1,12 @@
 import { error } from "@sveltejs/kit";
 import { supabase } from "$lib/data/supabase";
 import type { Event } from "$lib/data/myTypes";
+import { xyp_api_key, xyp_registration_url, xyp_portal_url } from '$env/static/private';
 
-export async function load({ params }) {
+export async function load({ params, parent }) {
   const result = await supabase.from("event").select().eq('id', params.id).single();
-  
+  const { cookie } = await parent();
+
   if (result.error) {
     console.error(result.error);
     throw error(500, {
@@ -12,6 +14,10 @@ export async function load({ params }) {
       });
   }
   return {
-    event: result.data as Event
+    event: result.data as Event,
+    cookie: cookie,
+    xyp_portal_url: xyp_portal_url,
+    xyp_api_key: xyp_api_key,
+    xyp_registration_url: xyp_registration_url
   };
 }
--- src/routes/(portal)/event/[id]/+page.svelte
+++ src/routes/(portal)/event/[id]/+page.svelte
@@ -1,37 +1,135 @@
 <script lang="ts">
+    import { supabase } from '$lib/data/supabase';
+
     import type { PageData } from './$types';
     import type { Event } from '$lib/data/myTypes';
 
     import { myProfile } from '$lib/data/myProfile';
+    import { myEvents } from '$lib/data/myEvents';    	
+    import { loadMyEvents } from '$lib/util/loadMyEvents';
 
     import { isAlreadyRegistered, buttonCheck } from '$lib/util/validationHelpers';
     import { trackAction } from '$lib/util/trackAction';
+    import { displayError, displayWarning, displaySuccess } from '$lib/util/displayToast';
 
     import SocialMedia from '$lib/components/SocialMedia.svelte';
     import CalendarButton from '$lib/components/CalendarButton.svelte';
+    import User from '$lib/components/User/User.svelte';
 
     export let data: PageData;
 
-    const registerEvent = (event:Event) => {
+    const registerXyp = async (x_id:string) => {
+		
+		if (!$myProfile) {
+			displayError('Missing user profile!');
+			return false;
+		}
 
+		if (!x_id) {
+			displayError('Missing XyvidPro Event ID!');
+			return false;
+		}
+		
+		//# currently the api will 500 if any of these are empty
+		// we shouldn't have empty values at this stage since all are required (?) when creating profile but still, brittle	
+		let x_body = {			
+			events: [ x_id ],
+			fname: $myProfile.first_name ? $myProfile.first_name : '.', //# if someday there is a weird bug with a bunch of '.' in data, i am so sorry
+			lname: $myProfile.last_name ? $myProfile.last_name : '.',
+			company: $myProfile.company ? $myProfile.company : '.',
+			email: $myProfile.email ? $myProfile.email : '.',
+			location: $myProfile.country ? $myProfile.country : '.',
+			joblevel: $myProfile.job_level ? $myProfile.job_level : '.',
+			jobtitle: $myProfile.job_title ? $myProfile.job_title : '.',
+		}		
+
+		// expected format:
+		// x_body = {
+		// 	"events": [
+		// 		"152109"
+		// 	],
+		// 	"fname": "Cailen",
+		// 	"lname": "Fisher",
+		// 	"company": "XYVID",    
+		// 	"email": "cfisher@xyvid.com",
+		// 	"location": "US",
+		// 	"joblevel": "Staff",
+		// 	"jobtitle": "Dev"
+		// }
+
+		let x_reg = await fetch(xyp_registration_url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				// 'Accept': '*/*',
+				'x-api-key': xyp_api_key
+			},
+			body: JSON.stringify(x_body),			
+		})
+		
+		if (x_reg.ok) {
+			let response = await x_reg.json();
+			
+			const { error } = await supabase
+				.from('attendee')
+				.update({ xyp_attnum: response[0].AttendeeID })
+				.eq('id', $myProfile.id);
+
+			if (error) {
+				displayError('Error updating XYP attnum after event registration!')
+				console.error(error);
+				return false;
+			}
+			return true;
+		} else {
+			displayError('Unknown error completing XYP registration!');
+			console.error(x_reg.statusText);
+		}						
+	}
+
+	const registerEvent = async (event:Event) => {
+		if (isAlreadyRegistered(event.id)) {			
+			return;
+		} 		
+		disableButton = true;
+		setTimeout(() => { disableButton = false }, 3000);
+		if (!buttonCheck(cookie)) {
+			return false;
+		}
+		// complete XYP registration first, only update records if success
+		if (! await registerXyp(event.xyp_id)) {
+			displayError('Error registering with Xyvid Pro');
+			return;
+		}
+		const { data, error } = await supabase
+			.from('registration')
+			.upsert({ event: event.id, attendee: $myProfile.id }) //# upsert not working as expected, figure out correct syntax (it allows dups)
+			.select();
+
+			if (error) {					
+				displayError(error.message);										
+			}
+			$myEvents = await loadMyEvents($myProfile.id);
+      alreadyRegistered = isAlreadyRegistered(event.id);
+			displaySuccess('You are registered!');
+	}
+
+    const actionGuide = () => {
+        if (buttonCheck(cookie)) {
+            trackAction(cookie ?? '', 'download_action_guide', event.id);
+            window.open(event.action_guide, '_blank');
+        } 
     }
 
-    // const actionGuide = () => {
-    //     if (buttonCheck(cookie)) {
-    //         trackAction(cookie ?? '', 'download_action_guide', event.id);
-    //         window.open(event.action_guide, '_blank');
-    //     } 
-    // }
+    const watchNow = async () => {
+        if (buttonCheck(cookie)) {
+            await registerEvent(event);
+            trackAction(cookie ?? '', 'watch_replay', event.id);
+            window.open(xyp_portal_url + event.xyp_id + '?emailAddress=' + $myProfile.email + '&directEntry=true', '_blank');            
+        }
+    }
 
-    // const watchNow = async () => {
-    //     if (buttonCheck(cookie)) {
-    //         await registerEvent(event);
-    //         trackAction(cookie ?? '', 'watch_replay', event.id);
-    //         window.open(xyp_portal_url + event.xyp_id + '?emailAddress=' + $myProfile.email + '&directEntry=true', '_blank');            
-    //     }
-    // }
-
-    let { event } = data;    
+    let { event, cookie, xyp_portal_url, xyp_api_key, xyp_registration_url  } = data;    
     let alreadyRegistered = isAlreadyRegistered(event.id);
     let disableButton = false;
 </script>
@@ -46,12 +144,12 @@
 </svelte:head>
 
 {#if event}
-<div class="flex flex-col md:flex-row w-full card rounded-lg">
-  <div class="rounded-t-lg md:rounded-none md:rounded-l-lg w-full">
-    <img src="{event.image_url}" alt="{event.title}" class="w-full" />
-  </div>
+<div class="flex flex-col md:flex-row w-full card rounded-lg">    
   
   <div class="flex flex-col justify-start p-6">
+    <div class="rounded-t-lg md:rounded-none md:rounded-l-lg w-full">
+      <img src="{event.image_url}" alt="{event.title}" class="w-full" />
+    </div>
     <h3
       class="mb-2 text-primary-500">
       {event.title}
@@ -106,6 +204,9 @@
         {@html event.content_replay}
         {/if}
     </p>
+  </div>
+  <div class="hidden md:inline w-1/3 bg-[#dedede] p-6">			
+    <User {cookie} />
   </div>
 </div>
 {:else}
