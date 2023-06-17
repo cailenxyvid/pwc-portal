9ae6560e60375f6e9e5855e430a385fa4e0642c3
xyvidbilling <53573032+xyvidbilling@users.noreply.github.com>
2023-06-16 22:59:49 -0400

    prettier

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-    import { blur } from "svelte/transition";
-    export let show = false;
-    const toggle = () => { show = !show }
+	import { blur } from 'svelte/transition';
+	export let show = false;
+	const toggle = () => {
+		show = !show;
+	};
 </script>
 
-<button on:click={toggle}><i class="inline-block fa fa-search pt-5 pb-5"></i></button>
+<button on:click={toggle}><i class="inline-block fa fa-search pt-5 pb-5" /></button>
 {#if show}
-<form action="/search" class="inline-block ml-1 sm:ml-4" transition:blur>
-    <input type="text" name="queryString" class="md:w-48 text-black" />
-    <button class="variant-filled-primary p-2">Go</button>
-</form>
+	<form action="/search" class="inline-block ml-1 sm:ml-4" transition:blur>
+		<input type="text" name="queryString" class="md:w-48 text-black" />
+		<button class="variant-filled-primary p-2">Go</button>
+	</form>
 {/if}
+
+<style>
+	button:hover,
+	.active {
+		color: #d04a02;
+	}
+</style>
