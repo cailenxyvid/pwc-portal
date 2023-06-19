dfb8258c767d601b4c9fe645811d3c807dec8251
xyvidbilling <53573032+xyvidbilling@users.noreply.github.com>
2023-06-18 23:29:36 -0400

    small and left-justified continue button +prettier

--- src/lib/components/User/LoginForm.svelte
+++ src/lib/components/User/LoginForm.svelte
@@ -1,4 +1,10 @@
-<form method="post" action="/login" class="text-center">
-    <input type="email" name="email" id="email" placeholder="email" class="sm:w-2/4 md:w-64 md:mb-2" />
-    <button type="submit" class="variant-filled-primary p-2 rounded-sm sm:w-2/4 md:w-64">Continue</button>    
-</form>	
+<form method="post" action="/login">
+	<input
+		type="email"
+		name="email"
+		id="email"
+		placeholder="Email address"
+		class="sm:w-2/4 md:w-64 md:mb-2"
+	/>
+	<button type="submit" class="btn bg-primary-500 text-white">Continue</button>
+</form>
--- src/lib/components/User/ModalLogin.svelte
+++ src/lib/components/User/ModalLogin.svelte
@@ -1,9 +1,8 @@
 <script>
-	import LoginForm from "./LoginForm.svelte";
-
+	import LoginForm from './LoginForm.svelte';
 </script>
 
 <div class="p-10 bg-white rounded-sm">
-    <h5>Please enter your email to continue with registration.</h5>
-    <LoginForm />
+	<div class="mb-4">Please enter your email to continue with registration.</div>
+	<LoginForm />
 </div>
