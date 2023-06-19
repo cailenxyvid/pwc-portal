cf2d735ef618b7e532ee91d6087eb5646e143cb1
xyvidbilling <53573032+xyvidbilling@users.noreply.github.com>
2023-06-18 22:35:19 -0400

    faq desgin cleanup +prettier

--- src/routes/(portal)/faq/+page.svelte
+++ src/routes/(portal)/faq/+page.svelte
@@ -3,18 +3,18 @@
 
 	import type { PageData } from './$types';
 
-  	export let data: PageData;
-	
+	export let data: PageData;
+
 	let { faqs } = data;
-    $: ({ faqs } = data);
+	$: ({ faqs } = data);
 </script>
-    
-<div class="container h-full mx-auto justify-center pt-2 pl-10 pr-10">
-    <h1 class="text-primary-500">Frequently Asked Questions</h1>
-	{#each faqs as faq}
-    <div class="p-8 border-b border-b-black">
-        <div id="faq-{faq.id}" class="text-xl">{faq.title}</div>
-        <div class="text-sm mt-2">{@html faq.content}</div>
-    </div>
+
+<div class="h-full p-8">
+	<h1 class="text-primary-500 mb-8">Frequently Asked Questions</h1>
+	{#each faqs as faq, i}
+		<div class="{i < faqs.length - 1 ? 'border-b border-b-primary-500' : ''} mb-2">
+			<div class="text-xl font-bold mb-2" id="faq-{faq.id}">{faq.title}</div>
+			<div class="mb-6">{@html faq.content}</div>
+		</div>
 	{/each}
 </div>
