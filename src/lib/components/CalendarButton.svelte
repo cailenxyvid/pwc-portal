0ee76561d0d44cbeeb62fd2bcd22be118f91d4aa
xyvidbilling <53573032+xyvidbilling@users.noreply.github.com>
2023-06-19 10:19:07 -0400

    standardized button size, +prettier

--- src/lib/components/CalendarButton.svelte
+++ src/lib/components/CalendarButton.svelte
@@ -1,28 +1,53 @@
 <script lang="ts">
-    import { clickOutside } from "$lib/util/clickOutside";
+	import { clickOutside } from '$lib/util/clickOutside';
 
-    import type { Event } from "$lib/data/myTypes";
+	import type { Event } from '$lib/data/myTypes';
 
-    export let event:Event;
+	export let event: Event;
 
-    let show = false;
+	let show = false;
 </script>
 
-{#if event.calendar_id}
-    <button class="text-xl variant-filled-primary text-white p-2 rounded-sm" on:click={() => { show = !show }}>
-        Add to calendar
-        <i class="fa-solid fa-chevron-{show ? 'up' : 'down'}"></i>
-    </button>
-    {#if show}
-        <div use:clickOutside on:outclick={() => { show = false }} class="p-6 rounded-sm absolute right-0 bg-surface-200">
-            <a href="https://www.addevent.com/event/{event.calendar_id}+outlook" target="_blank" class="block">Outlook</a>
-            <a href="https://www.addevent.com/event/{event.calendar_id}+google" target="_blank" class="block">Google</a>
-            <a href="https://www.addevent.com/event/{event.calendar_id}+apple" target="_blank" class="block">Apple</a>
-            {#if event.pwc_calendar_link}
-            <a href="{event.pwc_calendar_link}" target="_blank" class="block">PwC Desktop</a>
-            {/if}
-        </div>
-    {/if}
-    {:else}
-    <div class="text-xl variant-filled-error text-white">Missing calendar link!</div>
-{/if}
+<span class="relative">
+	{#if event.calendar_id}
+		<button
+			class="btn bg-primary-500 text-white mb-2"
+			on:click={() => {
+				show = !show;
+			}}
+		>
+			Add to calendar &nbsp;
+			<i class="fa-solid fa-chevron-{show ? 'up' : 'down'}" />
+		</button>
+		{#if show}
+			<div
+				use:clickOutside
+				on:outclick={() => {
+					show = false;
+				}}
+				class="p-6 rounded-sm absolute right-0 bg-surface-200"
+			>
+				<a
+					href="https://www.addevent.com/event/{event.calendar_id}+outlook"
+					target="_blank"
+					class="block">Outlook</a
+				>
+				<a
+					href="https://www.addevent.com/event/{event.calendar_id}+google"
+					target="_blank"
+					class="block">Google</a
+				>
+				<a
+					href="https://www.addevent.com/event/{event.calendar_id}+apple"
+					target="_blank"
+					class="block">Apple</a
+				>
+				{#if event.pwc_calendar_link}
+					<a href={event.pwc_calendar_link} target="_blank" class="block">PwC Desktop</a>
+				{/if}
+			</div>
+		{/if}
+	{:else}
+		<div class="text-xl variant-filled-error text-white">Missing calendar link!</div>
+	{/if}
+</span>
