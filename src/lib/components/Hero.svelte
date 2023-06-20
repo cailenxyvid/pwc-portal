d99df09c990652baababf7845822f8f59f8b1a8d
xyvidbilling <53573032+xyvidbilling@users.noreply.github.com>
2023-06-20 13:49:17 -0400

    made a hero component incase they want it on other pages like their mockup

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,10 @@
+<div class="flex flex-row w-full bg-[#dedede] p-0">
+	<div class="self-center p-4 md:p-6 lg:p-8 xl:p-12 2xl:p-16 mr-auto">
+		<div class="text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold">Trust in Action</div>
+		<div class="lg:text-lg xl:text-xl 2xl:text-2xl">Webcast series</div>
+	</div>
+	<div class="w-2/3">
+		<!-- <img src="/TLI-TIA-Header.png" alt=""> -->
+		<img src="header-cropped.png" alt="" />
+	</div>
+</div>
