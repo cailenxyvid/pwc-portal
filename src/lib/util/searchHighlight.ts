1c66823bb76ba4b7fb6becd55814024cb6caafde
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-21 13:14:37 -0400

    remove leading space requirement from search query

--- src/lib/util/searchHighlight.ts
+++ src/lib/util/searchHighlight.ts
@@ -1,7 +1,7 @@
 export const highlight = (text: string, query: string | null) => {
     if (!query) return;
-    let queryString = ' ' + query;
-    let regEx = new RegExp(queryString, 'ig');
+    // let queryString = ' ' + query;
+    let regEx = new RegExp(query, 'ig');
     let result = text.match(regEx)
     //# figure out logic to use all matches in output - taking the first result works most of the time, but isn't reliable 
     let output = result?.[0]
