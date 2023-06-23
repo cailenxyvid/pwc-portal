3e714b2704f4360f70df449a5c51b7f0fe096bd8
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-23 13:48:26 -0400

    XYPRO-933 update my events verbiage

--- src/lib/components/User/MyEvents.svelte
+++ src/lib/components/User/MyEvents.svelte
@@ -28,8 +28,8 @@
 
 	<div class="text-md border-t border-black mt-8">Past events</div>
 	<span class="text-sm block mb-6"
-		>Need your CPE certificate from a live webcast? Access the replay, click the "CPE" icon at the
-		bottom-right, and download it instantly.</span
+		>Need your CPE certificate from the live webcast you joined? Access the past event, click the
+		"CPE" icon at the bottom-right, and download it instantly.</span
 	>
 	{#if $myReplayEvents.length > 0}
 		{#each $myReplayEvents as row}
