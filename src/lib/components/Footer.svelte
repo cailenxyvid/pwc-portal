c50bbfcb7cecd866610de404e7f9cb3bb44b252c
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-26 17:19:54 -0400

    client requested footer updates

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,57 +1,29 @@
-<!-- <div class="mt-12 bg-[#2d2d2d] p-12">
-    <div class="flex flex-col md:flex-row justify-between">
-        <div class="text-white">
-            Have additional questions?
-            <span class="text-xl block">
-                <a href="mailto:us_trust_leadership_institute_inbox@pwc.com">Contact us</a> to 
-                learn more.
-            </span>
-        </div>
-        <div class="flex flex-row gap-4">
-            <img src="/jonathan-block.jpg" alt="" class="inline h-24">
-            <div class="t">
-                <h2 class="text-white">Jonathan Block</h2>
-                <div class="text-white">Communications and Change Leader, PwC US</div>
-            </div>			
-        </div>
-        <div class="flex flex-row gap-4">
-            <img src="/megan-conway.jpg" alt="" class="inline h-24">
-            <div class="t">
-                <h2 class="text-white">Megan Conway</h2>
-                <div class="text-white">Managing Director, Trust Leadership Institute</div>
-            </div>			
-        </div>
+<div class="mt-12 bg-[#2d2d2d] p-12 text-white">
+    <div>
+        Have questions? Want to learn more? Get in touch with our team. <a
+            href="mailto:us_trust_leadership_institute_inbox@pwc.com">Email us</a
+        >.
     </div>
-    <div class="text-xs text-white w-full mt-6">
-        © 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure for further details. This content is for general purposes only, and should not be used as a substitute for consultation with professional advisors.
-    </div>
-</div> -->
-
-<div class="bg-[#2d2d2d] text-white p-8 md:px-12">
-	<div class="flex flex-col container mx-auto">
-		<div>
-			<hr class="border-[#dedede] mb-2" />
-		</div>
-		<h3 class="mb-4">Connect with us</h3>
-		<div class="flex flex-row flex-wrap mb-16 gap-4 lg:gap-16 xl:gap-32">
-			<div>
-				<div class="text-xl block">Jonathan Block</div>
-				Managing Director, PwC’s Trust Leadership Institute
-			</div>
-			<div>
-				<div class="text-xl block">Megan Conway</div>
-				Managing Director, PwC’s Trust Leadership Institute
+	<div class="flex flex-col md:flex-row justify-between gap-4 my-4">		
+		<div class="flex flex-row gap-4">
+			<img src="/jonathan-block.jpg" alt="" class="inline h-24" />
+			<div class="t">
+				<h2>Jonathan Block</h2>
+				<div>Communications and Change Leader, PwC US</div>
 			</div>
 		</div>
-		<h3 class="mb-4">
-			Have additional questions?
-			<a href="mailto:us_trust_leadership_institute_inbox@pwc.com">Contact&nbsp;us</a> to learn more.
-		</h3>
-		<div class="text-xs">
-			© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to
-			the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure
-			for further details. This content is for general purposes only, and should not be used as a
-			substitute for consultation with professional advisors.
+		<div class="flex flex-row gap-4">
+			<img src="/megan-conway.jpg" alt="" class="inline h-24" />
+			<div>
+				<h2>Megan Conway Bley </h2>
+				<div>Managing Director, Trust Leadership Institute</div>
+			</div>
 		</div>
+	</div>
+	<div class="text-xs">
+		© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to
+		the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure
+		for further details. This content is for general purposes only, and should not be used as a
+		substitute for consultation with professional advisors.
 	</div>
 </div>
