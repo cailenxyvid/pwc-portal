1387e4cedeee0bee552d5b99c67541da9baeb902
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-28 11:31:19 -0400

    wrap footer content in container

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,29 +1,31 @@
-<div class="mt-12 bg-[#2d2d2d] text-white p-8 md:px-12 xl:px-16">
-    <div>
-        Have questions? Want to learn more? Get in touch with our team. <a
-            href="mailto:us_trust_leadership_institute_inbox@pwc.com">Email us</a
-        >.
-    </div>
-	<div class="flex flex-col md:flex-row justify-between gap-4 my-4">		
-		<div class="flex flex-row gap-4">
-			<img src="/jonathan-block.jpg" alt="" class="inline h-24" />
-			<div class="t">
-				<h2>Jonathan Block</h2>
-				<div>Communications and Change Leader, PwC US</div>
+<div class="mt-12 bg-[#2d2d2d] text-white p-8">
+	<div class="mx-auto container">
+		<div>
+			Have questions? Want to learn more? Get in touch with our team. <a
+				href="mailto:us_trust_leadership_institute_inbox@pwc.com">Email us</a
+			>.
+		</div>
+		<div class="flex flex-col md:flex-row justify-between gap-4 my-4">
+			<div class="flex flex-row gap-4">
+				<img src="/jonathan-block.jpg" alt="" class="inline h-24" />
+				<div class="t">
+					<h2>Jonathan Block</h2>
+					<div>Communications and Change Leader, PwC US</div>
+				</div>
+			</div>
+			<div class="flex flex-row gap-4">
+				<img src="/megan-conway.jpg" alt="" class="inline h-24" />
+				<div>
+					<h2>Megan Conway Bley</h2>
+					<div>Managing Director, Trust Leadership Institute</div>
+				</div>
 			</div>
 		</div>
-		<div class="flex flex-row gap-4">
-			<img src="/megan-conway.jpg" alt="" class="inline h-24" />
-			<div>
-				<h2>Megan Conway Bley</h2>
-				<div>Managing Director, Trust Leadership Institute</div>
-			</div>
+		<div class="text-xs">
+			© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to
+			the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure
+			for further details. This content is for general purposes only, and should not be used as a
+			substitute for consultation with professional advisors.
 		</div>
-	</div>
-	<div class="text-xs">
-		© 2023 PwC. All rights reserved. PwC refers to the US member firm, and may sometimes refer to
-		the PwC network. Each member firm is a separate legal entity. Please see www.pwc.com/structure
-		for further details. This content is for general purposes only, and should not be used as a
-		substitute for consultation with professional advisors.
 	</div>
 </div>
