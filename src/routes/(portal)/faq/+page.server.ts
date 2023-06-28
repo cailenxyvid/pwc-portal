e9630c7ba79bf83da304211fe8a66cb4ef426367
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-28 10:08:32 -0400

    sort faq items

--- src/routes/(portal)/faq/+page.server.ts
+++ src/routes/(portal)/faq/+page.server.ts
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/data/supabase"
 
 export async function load() {
-  const { data } = await supabase.from("faq").select();  
+  const { data } = await supabase.from("faq").select().order('display_order');  
   return {
     faqs: data ?? [],
   };
