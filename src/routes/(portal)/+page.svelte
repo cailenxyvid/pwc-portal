8c02447252013709ebef1b8617eacbcb509830d5
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-06-29 11:01:13 -0400

    thin rule above past events

--- src/routes/(portal)/+page.svelte
+++ src/routes/(portal)/+page.svelte
@@ -60,7 +60,8 @@
 		</div>
 	</div>
 
-	<div class="p-8 md:p-16">
+	<div class="p-8 pt-0 mt-4 md:p-16 ">
+		<div class="border-t border-black"></div>
 		<h2 class="my-12">Past events</h2>
 		<div
 			class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-x-10 lg:gap-x-20 2xl:gap-x-30 gap-y-5 lg:gap-y-10 2xl:gap-y-20"
