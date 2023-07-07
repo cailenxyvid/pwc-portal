073b845a1e680b0bda248fc7f8cbf4cc5cccd7de
cailenxyvid <103433837+cailenxyvid@users.noreply.github.com>
2023-07-07 13:26:25 -0400

    XYPRO-966 corrected regex in search result highlighting to exlude matches inside html tags

--- src/lib/util/searchHighlight.ts
+++ src/lib/util/searchHighlight.ts
@@ -1,10 +1,28 @@
 export const highlight = (text: string, query: string | null) => {
-    if (!query) return;
-    // let queryString = ' ' + query;
-    let regEx = new RegExp(query, 'ig');
-    let result = text.match(regEx)
-    //# figure out logic to use all matches in output - taking the first result works most of the time, but isn't reliable 
-    let output = result?.[0]
+    if (!query) return text;
+  
+    let regEx = new RegExp(`(?!<[^>]+)${query}(?![^<]*?>)`, 'ig');
+    let result = text.match(regEx);
+  
+    if (!result) return text;
+  
+    let outputText = text.replace(regEx, `<b class="highlight">$&</b>`);
+  
+    return outputText;
+  };
+  
+
+// export const highlight = (text: string, query: string | null) => {
+//     if (!query) return;
+//     // let queryString = ' ' + query;
+
+
+//     let regEx = new RegExp(query, 'ig');
     
-    return text.replace(regEx, `<b class="highlight">${output}</b>`);
-};
+    
+//     let result = text.match(regEx)
+//     //# figure out logic to use all matches in output - taking the first result works most of the time, but isn't reliable 
+//     let output = result?.[0]
+    
+//     return text.replace(regEx, `<b class="highlight">${output}</b>`);
+// };
